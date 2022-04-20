365206aa3223e1c510e2c75407e3ed57ca5be919
omkar-parkhi <76740351+omkar-parkhi@users.noreply.github.com>
2022-04-20 12:51:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+$(".owl-carousel").owlCarousel({
+	autoplay:true,
+	autoplayHoverPause:true,
+	items:3,
+	dots:true,
+	loop:true,
+	lazyload:true,
+	margin:10,
+	stagePadding:5,
+	responsive:{
+		0:{
+			items:1,
+			dots:false	
+		},
+		485:{
+			items:1,
+			dots:false	
+		},
+		728:{
+			items:2,
+			loop:true
+		},
+		960:{
+			items:3,
+			loop:true	
+		},
+		1200:{
+			items:3,
+			dots:true	
+		}
+	}
+	});
